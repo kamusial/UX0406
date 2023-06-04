250df298b209f6fda43f0c1ad39e553061824e58
musiakam <kamil.musial@tietoevry.com>
2023-06-04 09:55:49 +0200

    1

--- 1_part1/5_button_click.py
+++ 1_part1/5_button_click.py
@@ -8,8 +8,16 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Moja aplikacja")
         button = QPushButton("przycisk")
+        button.clicked.connect(self.button_clicked)
+        button.released.connect(self.button_released)
         self.setFixedSize(QSize(600, 200))
         self.setCentralWidget(button)
+
+    def button_clicked(self):
+        print('Wcisniety')
+
+    def button_released(self):
+        print('Puszczony')
 
 app = QApplication(sys.argv)
 window = MainWindow()
