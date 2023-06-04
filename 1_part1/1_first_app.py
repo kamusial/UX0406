9883c4a0b2b2ac8288075ea8c5abf54d3c4b1ae0
musiakam <kamil.musial@tietoevry.com>
2023-06-04 12:03:28 +0200

    1

--- 1_part1/1_first_app.py
+++ 1_part1/1_first_app.py
@@ -1,7 +1,15 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QDialog
 import sys
 
 app = QApplication(sys.argv)
-window = QWidget()
+
+window = QDialog()
+
+#window = QWidget()
+
+# window = QMainWindow()
+# window.statusBar().showMessage("Status....")
+# window.menuBar().addMenu("Options")
+
 window.show()
 app.exec()
---
+++ 2_part2/1_differenecs.py
@@ -0,0 +1,8 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+import sys
+
+app = QApplication(sys.argv)
+#window = QWidget()
+
+window.show()
+app.exec()
