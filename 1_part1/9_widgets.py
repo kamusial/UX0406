1d16a870a1475b77043ffbd8ef56605e28aa4ea6
musiakam <kamil.musial@tietoevry.com>
2023-06-04 10:57:40 +0200

    1

---
+++ 1_part1/9_widgets.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget,
+)
