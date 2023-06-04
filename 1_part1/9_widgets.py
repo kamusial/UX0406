409c9cffb628d9e8a078b69ecb777c995e83a857
musiakam <kamil.musial@tietoevry.com>
2023-06-04 11:08:42 +0200

    1

--- 1_part1/9_widgets.py
+++ 1_part1/9_widgets.py
@@ -21,3 +21,40 @@ from PyQt6.QtWidgets import (
     QVBoxLayout,
     QWidget,
 )
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Widgets App")
+        layout = QVBoxLayout()
+        widgets = [
+            QCheckBox,
+            QComboBox,
+            QDateEdit,
+            QDateTimeEdit,
+            QDial,
+            QDoubleSpinBox,
+            QFontComboBox,
+            QLCDNumber,
+            QLabel,
+            QLineEdit,
+            QProgressBar,
+            QPushButton,
+            QRadioButton,
+            QSlider,
+            QSpinBox,
+            QTimeEdit,
+        ]
+
+        for w in widgets:
+            layout.addWidget(w())
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
